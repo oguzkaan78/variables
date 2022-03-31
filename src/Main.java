29299fc7f2deddecc68bb93237e9a0f1f37c5a50
oguzkaan78 <gezginler78@hotmail.com>
2022-03-31 20:35:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int benimyas = 19;
+        String yasmetni = "Şu anda yaşım : ";
+
+        System.out.println(yasmetni + benimyas);
+    }
+}
